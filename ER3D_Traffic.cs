b333b607f30747be38bf33fa19222951a9c700b1
James Rose <blissgig@gmail.com>
2019-12-07 12:56:36 -0500

    The two main files for this process

---
+++ ER3D_Traffic.cs
@@ -0,0 +1,355 @@
+﻿using UnityEngine;
+using EasyRoads3Dv3;
+using Unity.Entities;
+using System.Collections.Generic;
+using Unity.Transforms;
+using Unity.Mathematics;
+using System.Linq;
+
+public class ER3D_Traffic : MonoBehaviour
+{
+    [Header("Autos")]
+    [SerializeField]
+    private List<GameObject> autos = new List<GameObject>();
+
+    [Header("Settings")]
+    [SerializeField]
+    private float speedMinimum = 10;
+
+    [SerializeField]
+    private float speedMaximum = 40;
+
+    [SerializeField]
+    private int vehicleLength = 3;
+
+    [SerializeField]
+    private int percentageLanesPopulated = 100;
+
+
+    private EntityManager entityManager;
+    private ConnectedTo connectedTo;
+    private List<Entity> autoEntities = new List<Entity>();
+
+
+    private void Start()
+    {
+        entityManager = World.Active.EntityManager;
+
+        if (percentageLanesPopulated > 100)
+        {
+            //Percentage can't be over 100, duh.
+            percentageLanesPopulated = 100;  
+        }
+
+        AddIndentityMono();
+
+        CreateAutoEntities();
+
+        CreateRoadEntities();
+
+        //TODO: remove mono identity after the road/connection entities have been created.
+    }
+
+    private void CreateAutoEntities()
+    {
+        /* Because of allowing the input of GameObjects and the 
+         * time cost to converting them, this function is to create 
+         * instances of the Entities, and those are used in the 
+         * random selection in CreateAutoEntity.   
+         * Saves TONS of time, like MINUTES worth.
+         * eg: 3000 instances took 2 minutes, vs less than half a second
+         */
+
+        foreach (GameObject auto in autos)
+        {
+            Entity prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(auto, World.Active);
+            var entity = entityManager.Instantiate(prefab);
+
+            entityManager.AddComponent(entity, typeof(ERAutoTag));
+            entityManager.AddComponent(entity, typeof(AutoPosition));
+            entityManager.AddComponent(entity, typeof(AutoDetails));
+            entityManager.AddComponent(entity, typeof(AutoLanePoints));
+
+            autoEntities.Add(entity);
+        }
+    }
+
+    private void CreateRoadEntities()
+    {
+        var roadEntityArchetype = entityManager.CreateArchetype(
+            typeof(ERRoadTag),
+            typeof(RoadDetails),
+            typeof(LanePoints)
+            );
+
+        var connectionEntityArchetype = entityManager.CreateArchetype(
+            typeof(ERConnectionTag),
+            typeof(ConnectionDetails),
+            typeof(LanePoints)
+        );
+
+        int nextRoadIdentity = 0;
+        int nextLaneIndex = 0;
+        int connectionIndex;
+        int connectionIdentityEnd = 0;
+        int connectionIdentityStart = 0;
+        ERConnection erConnectionEnd;
+        ERConnection erConnectionStart;
+        ERRoadNetwork roadNetwork = new ERRoadNetwork();
+        ERRoad[] roads = roadNetwork.GetRoads();
+        List<Vector3> allLanePoints = new List<Vector3>();
+        List<Vector3> connectionLanePoints = new List<Vector3>();
+        ConnectedTo connectedTo;
+
+
+        //Make entities for each of these objects
+        //Roads first.  They need a collection of Lanes and each lane has an input and output connection identity.
+        //This collection of Road Identity & Lane Identity, or Connection Identity & Lane Identity
+        //will be used in the JOB to get the next load of data
+        foreach (ERRoad road in roads)
+        {
+            var roadLevelData = road.gameObject.GetComponent<ERRoadLevelDataHolder>().levelData;
+            int roadIdentity = road.gameObject.GetComponent<ERRoadConnectionIdentity>().value;
+            int lanes = road.GetLaneCount();
+            
+
+            for (int lane = 0; lane < lanes; lane++)
+            {
+                ERLaneData erLaneData = road.GetLaneData(lane);
+                Vector3[] roadLanePoints = erLaneData.points;
+
+                if (erLaneData.direction == ERLaneDirection.Right)
+                {
+                    erConnectionEnd = road.GetConnectionAtEnd(out connectionIndex);
+                    erConnectionStart = road.GetConnectionAtStart(out connectionIdentityStart);
+                }
+                else
+                {
+                    erConnectionEnd = road.GetConnectionAtStart(out connectionIndex);
+                    erConnectionStart = road.GetConnectionAtEnd(out connectionIdentityStart);                    
+                }
+
+                connectionIdentityStart = RoadConnectionIdentity(erConnectionStart.gameObject);
+                connectionIdentityEnd = RoadConnectionIdentity(erConnectionEnd.gameObject);
+
+                //Create the Road/Lane entity
+                Entity roadEntity = entityManager.CreateEntity(roadEntityArchetype);
+                entityManager.SetComponentData(
+                    roadEntity,
+                    new RoadDetails
+                    {
+                        RoadIdentity = roadIdentity,
+                        LaneIndex = erLaneData.laneIndex,
+                        ConnectionIdentityEnd = connectionIdentityEnd,
+                        ConnectionIdentityStart = connectionIdentityStart
+                    });
+
+                //Get and store the road's lane points
+                var roadLanePointsBuffer = entityManager.GetBuffer<LanePoints>(roadEntity);
+                var roadLanePoint = new LanePoints();
+
+                for (int point = 0; point < roadLanePoints.Length; point++)
+                {
+                    roadLanePoint.value = roadLanePoints[point];
+                    roadLanePointsBuffer.Add(roadLanePoint);
+                }
+                //---------------------------------------------------
+
+                //Create this Road/Lane/Connection combo
+                ERLaneConnector[] laneConnectors = erConnectionEnd.GetLaneData(connectionIndex, erLaneData.laneIndex);
+
+                for (int i = 0; i < laneConnectors.Length; i++)
+                {
+                    var laneConnector = laneConnectors[i];
+
+                    var endRoad = erConnectionEnd.GetConnectedRoad(laneConnector.endConnectionIndex, out connectedTo);
+                    var roadIdentityEnd = RoadConnectionIdentity(endRoad.gameObject);
+
+                    Entity connectionEntity = entityManager.CreateEntity(connectionEntityArchetype);
+                    entityManager.SetComponentData(
+                        connectionEntity,
+                        new ConnectionDetails
+                        {
+                            ConnectionIdentity = connectionIdentityEnd,
+                            LaneIndexStart = erLaneData.laneIndex,
+                            LaneIndexEnd = laneConnector.endLaneIndex,
+                            RoadIdentityStart = roadIdentity,
+                            RoadIdentityEnd = roadIdentityEnd,
+                        });
+
+                    var connectionLanePointsBuffer = entityManager.GetBuffer<LanePoints>(connectionEntity);
+                    var connectionLanePoint = new LanePoints();
+
+                    for (int x = 0; x < laneConnector.points.Length; x++)
+                    {
+                        connectionLanePoint.value = laneConnector.points[x];
+                        connectionLanePointsBuffer.Add(connectionLanePoint);
+                    }
+                }
+                //---------------------------------------------------
+                                
+                //Pick a connection and one of the output values to give to the cars that are about to be created
+                int randomValue = UnityEngine.Random.Range(0, laneConnectors.Length);
+                var laneConnect = laneConnectors[randomValue];
+                var exitRoad = erConnectionEnd.GetConnectedRoad(laneConnect.endConnectionIndex, out connectedTo);
+
+                nextLaneIndex = laneConnect.endLaneIndex;
+                nextRoadIdentity = RoadConnectionIdentity(exitRoad.gameObject);
+                connectionLanePoints = laneConnect.points.ToList();
+
+                allLanePoints.Clear();
+                allLanePoints.AddRange(roadLanePoints);
+                allLanePoints.AddRange(connectionLanePoints);
+                connectionLanePoints.Clear();
+
+                //Create a car for each level
+                for (int level = 0; level < roadLevelData.levelCount; level++)
+                {
+                    //Option to not fill every lane 
+                    int random = UnityEngine.Random.Range(0, 100);
+
+                    if (random <= percentageLanesPopulated)
+                    {
+                        CreateAutoEntity(
+                            allLanePoints,
+                            level,
+                            nextLaneIndex,
+                            nextRoadIdentity,
+                            connectionIdentityEnd,
+                            roadLevelData);
+                    }
+                }
+            }
+        }
+
+        allLanePoints.Clear();
+        connectionLanePoints.Clear();
+    }
+
+    private void CreateConnectionEntity(
+        ERConnection erConnection,
+        int connectionIndexStart,
+        int roadIdentityStart,
+        int laneIndex)
+    {
+        var entityArchetype = entityManager.CreateArchetype(
+                    typeof(ERConnectionTag),
+                    typeof(ConnectionDetails),
+                    typeof(LanePoints)
+                );
+            
+        ConnectedTo connectedTo;
+        int connectionIdentity = RoadConnectionIdentity(erConnection.gameObject);
+        ERLaneConnector[] laneConnectors = erConnection.GetLaneData(connectionIndexStart, laneIndex);
+            
+        if (laneConnectors != null)
+        {
+            for (int i = 0; i < laneConnectors.Length; i++)
+            {
+                var laneConnector = laneConnectors[i];
+                var nextLaneIndex = laneConnector.endLaneIndex;
+                var connectionLanePoints = laneConnector.points;
+                var endRoad = erConnection.GetConnectedRoad(laneConnector.endConnectionIndex, out connectedTo);
+                var roadIdentityEnd = RoadConnectionIdentity(endRoad.gameObject);
+                Entity entity = entityManager.CreateEntity(entityArchetype);
+                entityManager.SetComponentData(
+                    entity,
+                    new ConnectionDetails
+                    {
+                        ConnectionIdentity = connectionIdentity,
+                        LaneIndexStart = laneConnector.startLaneIndex,
+                        LaneIndexEnd = laneConnector.endLaneIndex,
+                        RoadIdentityStart = roadIdentityStart,
+                        RoadIdentityEnd = roadIdentityEnd,
+                    });
+            }
+        }
+    }
+
+    private void CreateAutoEntity(
+        List<Vector3> lanePoints,
+        int level,
+        int nextLaneIdentity,
+        int nextRoadIdentity,
+        int nextConnectionIdentity,
+        ERRoadLevelData roadLevelData)
+    {
+        Entity prefab = autoEntities[UnityEngine.Random.Range(0, autoEntities.Count - 1)];
+        var entity = entityManager.Instantiate(prefab);
+       
+        float speed = UnityEngine.Random.Range(speedMinimum, speedMaximum);
+        float yOffset = roadLevelData.startHeight + ((level + 1) * roadLevelData.levelHeight);
+        int currentIndex = UnityEngine.Random.Range(0, lanePoints.Count - vehicleLength);
+
+        Vector3 translation = lanePoints[currentIndex];
+        translation.y += yOffset;
+
+        Vector3 destination = lanePoints[currentIndex + 1];
+        destination.y += yOffset;
+
+        float3 lookVector = destination - translation;
+        Quaternion rotation = Quaternion.LookRotation(lookVector);
+
+        entityManager.SetComponentData(entity, 
+            new AutoDetails {
+            yOffset = yOffset,
+            speed = speed });
+
+        entityManager.SetComponentData(entity, 
+            new AutoPosition
+            {
+            CurrentPositionIndex = currentIndex,
+            Destination = destination,
+            LaneIndex = nextLaneIdentity,
+            RoadIdentity = nextRoadIdentity,
+            ConnectionIdentity = nextConnectionIdentity
+            });
+
+        entityManager.SetComponentData(entity, new Translation { Value = translation });
+        entityManager.SetComponentData(entity, new Rotation { Value = rotation });
+
+        var lanePointsBuffer = entityManager.GetBuffer<AutoLanePoints>(entity);
+        var autoLanePoints = new AutoLanePoints();
+
+        for (int point = 0; point < lanePoints.Count; point++)
+        {
+            autoLanePoints.value = lanePoints[point];
+            lanePointsBuffer.Add(autoLanePoints);
+        }
+    }
+
+    private void AddIndentityMono()
+    {
+        /*
+         * Set up a unique value for each Road and Connection.
+         * This is because we cannot use the Road or Connection name in ECS as strings are not allowed (only bittable data can be used)
+         * AND because road and connection names may not be unique.
+         * We need the unique value to identify the relationship between road and connection
+         */
+
+        ERRoadNetwork roadNetwork = new ERRoadNetwork();
+        ERRoad[] roads = roadNetwork.GetRoads();
+        ERConnection[] connections = roadNetwork.GetConnections();
+
+        int identity = 0;
+
+        foreach (ERRoad road in roads)
+        {
+            road.gameObject.AddComponent<ERRoadConnectionIdentity>();
+            road.gameObject.GetComponent<ERRoadConnectionIdentity>().value = identity;
+            identity++;
+        }
+
+        foreach (ERConnection connection in connections)
+        {
+            connection.gameObject.AddComponent<ERRoadConnectionIdentity>();
+            connection.gameObject.GetComponent<ERRoadConnectionIdentity>().value = identity;
+            identity++;
+        }
+    }
+
+    private int RoadConnectionIdentity(GameObject gameObject)
+    {
+        return gameObject.GetComponent<ERRoadConnectionIdentity>().value;
+    }   
+}
---
+++ ER3D_TrafficSystem.cs
@@ -0,0 +1,218 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class ER3D_TrafficSystem : JobComponentSystem
+{
+    //protected override void OnCreate()
+    //{
+    //    EntityQuery allRoadsQuery = GetEntityQuery(
+    //        typeof(ERRoadTag),
+    //        ComponentType.ReadOnly<RoadLaneIdentityComponent>(),
+    //        ComponentType.ReadOnly<IsLaneDirectionLeftComponent>(),
+    //        ComponentType.ReadOnly<LanePointsComponent>());
+
+    //    roadEntities = allRoadsQuery.ToEntityArray(Allocator.Persistent);
+
+    //    EntityQuery allConnectionsQuery = GetEntityQuery(
+    //        typeof(ERConnectionTag),
+    //        ComponentType.ReadOnly<ConnectionDetails>(),
+    //        ComponentType.ReadOnly<LanePointsComponent>());
+
+    //    //connectionEntities = allConnectionsQuery.ToEntityArray(Allocator.Persistent);
+    //}
+
+    [BurstCompile]
+    [RequireComponentTag(typeof(ERAutoTag))]
+    protected struct AutoNavigationJob : IJobForEach_BCCCC<
+        AutoLanePoints,
+        AutoDetails,
+        AutoPosition,
+        Translation,
+        Rotation>
+    {
+        [ReadOnly]
+        public ComponentDataFromEntity<RoadDetails> RoadDetailsFromEntity;
+
+        [ReadOnly]
+        public ComponentDataFromEntity<ConnectionDetails> ConnectionDetailsFromEntity;
+
+        [ReadOnly]
+        public BufferFromEntity<LanePoints> LanePointsFromEntity;
+
+        [ReadOnly]
+        public NativeArray<Entity> roadEntities;
+
+        [ReadOnly]
+        public NativeArray<Entity> connectionEntities;
+
+        public float deltaTime;
+        public float reachedPositionDistance;
+
+
+        public void Execute(
+            DynamicBuffer<AutoLanePoints> autoLanePoints, 
+            [ReadOnly] ref AutoDetails autoDetails, 
+            ref AutoPosition autoPosition,
+            ref Translation autoTranslation, 
+            ref Rotation autoRotation)
+        {
+            if (autoLanePoints.Length == 0)
+            {
+                /* This is a basic check, 
+                 * there seems to be a few instances 
+                 * where the auto does not have points, 
+                 */ 
+                return; 
+            }
+
+            var distance = math.distance(autoPosition.Destination, autoTranslation.Value);
+            int connectionIdentityEnd = 0;
+
+            if (distance < reachedPositionDistance)
+            {
+                autoPosition.CurrentPositionIndex += 1;
+
+                //If the Auto's current position is at the end of it's list of 
+                //points, get the next list of points from the road or connection.
+                if (autoPosition.CurrentPositionIndex >= autoLanePoints.Length)
+                {
+                    autoPosition.CurrentPositionIndex = 0;
+                    autoLanePoints.Clear();
+
+                    int laneIndex = 0;
+                    int roadIdentity = 0;
+
+
+                    for (int i = 0; i < roadEntities.Length; i++)
+                    {
+                        RoadDetails roadDetails = RoadDetailsFromEntity[roadEntities[i]];
+
+                        if ((roadDetails.RoadIdentity == autoPosition.RoadIdentity) &&
+                            (roadDetails.LaneIndex == autoPosition.LaneIndex) &&
+                            (roadDetails.ConnectionIdentityStart == autoPosition.ConnectionIdentity)
+                            )
+                        {
+                            laneIndex = roadDetails.LaneIndex;
+                            roadIdentity = roadDetails.RoadIdentity;
+                            connectionIdentityEnd = roadDetails.ConnectionIdentityEnd;
+
+                            var roadLanePointsBuffer = LanePointsFromEntity[roadEntities[i]];
+                            var roadLanePoints = roadLanePointsBuffer.ToNativeArray(Allocator.Temp);
+                            var lanePointAuto = new AutoLanePoints();
+
+                            for (int x = 0; x < roadLanePoints.Length; x++)
+                            {
+                                lanePointAuto.value = roadLanePoints[x].value;
+                                autoLanePoints.Add(lanePointAuto);
+                            }
+                            break;
+                        }
+                    }
+
+
+                    //Get the Connection's Lane's points
+                    //First find all options that have the same Idenity and Index.  
+                    //Then we can randomly select one of the routes through the connection
+                    NativeList<Entity> availableConnections = new NativeList<Entity>(Allocator.Temp);
+
+                    for (int i = 0; i < connectionEntities.Length; i++)
+                    {
+                        var connectionDetails = ConnectionDetailsFromEntity[connectionEntities[i]];
+
+                        if ((connectionDetails.ConnectionIdentity == connectionIdentityEnd) &&
+                            (connectionDetails.RoadIdentityStart == roadIdentity) && 
+                            (connectionDetails.LaneIndexStart == laneIndex)
+                            ) 
+                        {
+                            availableConnections.Add(connectionEntities[i]);
+                        }
+                    }
+                    
+                    uint seed = 88;
+                    Unity.Mathematics.Random mathRandom = new Unity.Mathematics.Random(seed);
+                    int randomValue = mathRandom.NextInt(0, availableConnections.Length - 1);
+                    var connectionDetailsNew = ConnectionDetailsFromEntity[availableConnections[randomValue]];
+                    var connectionLanePointsBuffer = LanePointsFromEntity[availableConnections[randomValue]];
+                    var connectionLanePoints = connectionLanePointsBuffer.ToNativeArray(Allocator.Temp);
+                    var lanePoint = new AutoLanePoints();
+
+                    for (int x = 0; x < connectionLanePoints.Length; x++)
+                    {   
+                        lanePoint.value = connectionLanePoints[x].value;
+                        autoLanePoints.Add(lanePoint);
+                    }
+                    autoPosition.LaneIndex = connectionDetailsNew.LaneIndexEnd;
+                    autoPosition.RoadIdentity = connectionDetailsNew.RoadIdentityEnd;
+                    autoPosition.ConnectionIdentity = connectionDetailsNew.ConnectionIdentity;
+
+                    availableConnections.Dispose();
+                }
+
+                autoPosition.Destination = autoLanePoints[autoPosition.CurrentPositionIndex].value;
+                autoPosition.Destination.y += autoDetails.yOffset;
+            }
+
+            float3 lookVector = autoPosition.Destination - autoTranslation.Value;
+
+            if (!lookVector.Equals(float3.zero))
+            {
+                Quaternion rotationLookAt = Quaternion.LookRotation(lookVector);
+                autoRotation.Value = rotationLookAt;
+            }
+            
+            float3 smoothedPosition = math.lerp(autoTranslation.Value, autoPosition.Destination, autoDetails.speed * deltaTime);
+            autoTranslation.Value = smoothedPosition;
+        }
+    }
+
+    private const float REACHED_POSITION_DISTANCE = 1.0f;
+    private NativeArray<Entity> roadEntities;
+    private NativeArray<Entity> connectionEntities;
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        //This is done here beacuse
+        // A) Only want to run the query once, these values are created and NEVER modified at run-time
+        // B) The data may not be available OnCreate.  TODO: attempt to place this there.
+        if (roadEntities.Length == 0)
+        {
+            EntityQuery allRoadsQuery = GetEntityQuery(
+                typeof(ERRoadTag),
+                ComponentType.ReadOnly<RoadDetails>(),
+                ComponentType.ReadOnly<LanePoints>());
+
+            roadEntities = allRoadsQuery.ToEntityArray(Allocator.Persistent);
+
+            EntityQuery allConnectionsQuery = GetEntityQuery(
+                typeof(ERConnectionTag),
+                ComponentType.ReadOnly<ConnectionDetails>(),
+                ComponentType.ReadOnly<LanePoints>());
+
+            //TODO: this SEEMS to be causing a memory leak, as reported by Unity.
+            connectionEntities = allConnectionsQuery.ToEntityArray(Allocator.Persistent);
+        }
+
+
+        AutoNavigationJob autoNaviationJob = new AutoNavigationJob
+        {
+            RoadDetailsFromEntity = GetComponentDataFromEntity<RoadDetails>(true),
+            ConnectionDetailsFromEntity = GetComponentDataFromEntity<ConnectionDetails>(true),
+            LanePointsFromEntity = GetBufferFromEntity<LanePoints>(true),
+            roadEntities = roadEntities,
+            connectionEntities = connectionEntities,
+            deltaTime = Time.deltaTime,
+            reachedPositionDistance = REACHED_POSITION_DISTANCE
+        };
+
+        JobHandle jobHandle = autoNaviationJob.Schedule(this, inputDeps);
+
+        //JobHandle jobHandle = autoNaviationJob.Run(this, inputDeps);  //USED FOR DEBUGGING
+
+        return jobHandle;
+    }
+}
